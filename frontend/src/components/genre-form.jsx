0748c8c1b93e4bfabaaa25a63961be343124e309
Shree Kottes <kottezh@gmail.com>
2024-10-22 22:50:26 +0530

    update packages

--- frontend/src/components/genre-form.jsx
+++
@@ -1,3 +0,0 @@
-const GenreForm = () => {};
-
-export default GenreForm;
--- frontend/src/components/song-form.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import axios from "axios";
-import { z } from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-
-const SongForm = () => {};
-
-export default SongForm;
